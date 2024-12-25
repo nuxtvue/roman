866ecbde59579f735ce9eb900e795db0f8c21183
nuxtvue <nuxtvue@yandex.ru>
2024-12-25 19:56:01 +0300

    fix

--- client/src/components/TopMenu.jsx
+++ client/src/components/TopMenu.jsx
@@ -3,7 +3,11 @@ const TopMenu = () => {
   const handleClick = () => {};
   return (
     <div className="flex flex-row justify-between items-center gap-4 mt-4 mx-8 text-lg">
-      <div>Главная</div>
+      <div className="flex flex-row gap-4">
+        <div>Логотип</div>
+        <div>Главная</div>
+      </div>
+
       <div id="userSettings" className="flex gap-6">
         <div className="flex gap-2 items-center" onClick={handleClick}>
           <MdLogin />
