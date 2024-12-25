1539a62318cdbed50d2b474aaef0f72e61b434a9
nuxtvue <nuxtvue@yandex.ru>
2024-12-25 14:24:18 +0300

    12312313

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          aaasdasd
+         asdasd
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
