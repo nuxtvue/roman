c4e29c11d69e3eeca73712c2cd4bcefffa7b706f
nuxtvue <nuxtvue@yandex.ru>
2024-12-27 10:48:29 +0300

    cors fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://62.109.13.68/"],
   credentials: true,
 };
 mongoose
